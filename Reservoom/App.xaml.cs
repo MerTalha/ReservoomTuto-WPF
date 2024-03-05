addd649e0855273a870cb97dd7b007ad02aa7b75
Mert Talha Şimşek <merttalhasimsek@gmail.com>
2024-03-05 14:28:54 +0300

    Model Update

--- Reservoom/App.xaml.cs
+++ Reservoom/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Reservoom.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,30 @@ namespace Reservoom
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Hotel hotel = new Hotel("Hotel Suite");
+            try
+            {
+                hotel.MakeReservation(new Reservation(
+                    new RoomID(1, 3),
+                    "HotelSuits",
+                    new DateTime(2000, 1, 1),
+                    new DateTime(2000, 1, 3)));
+
+                hotel.MakeReservation(new Reservation(
+                    new RoomID(1, 2),
+                    "HotelSuits",
+                    new DateTime(2000, 1, 5),
+                    new DateTime(2000, 1, 7)));
+
+            }catch (Exception ex) { }
+
+            
+
+            IEnumerable<Reservation> reservations = hotel.GetReservationForUser("HotelUser");
+
+            base.OnStartup(e);
+        }
     }
 }
